ae24f43066cc1aedba10978877f61b0faae53695
9Health <ninehealth at outlook dot com>
2022-06-01 09:40:21 +0700

    Get data from SQL, output to JSON and create a food card by using taiga API

--- 02_db_select.py
+++ 02_db_select.py
@@ -4,6 +4,8 @@
 #  https://www.postgresqltutorial.com/postgresql-python/
 
 import psycopg2
+import json
+import requests
 
 def select():
 
@@ -69,7 +71,36 @@ def select():
 
         with conn.cursor() as curs:
             curs.execute(sql2)
-            print(curs.fetchall())
+            # print(curs.fetchall())
+            # for row in curs.fetchmany(20):
+            #     print (row[1])
+            # print (curs.fetchmany(20))
+            # print (type(curs.fetchmany(20)))
+
+            row = curs.fetchmany(1)
+
+            payload = json.dumps( {
+                "project" : "3"       ,
+                "subject" : row[0][1]
+            } )
+
+            print (payload)
+
+            url = "http://9net.ddns.net:9000/api/v1/userstories"
+
+            AUTH_TOKEN = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjU0MDc5NDA0LCJqdGkiOiI4YzdlY2ZmOTA0YTI0MmJhYmMzNzA2MGEzMzkyYmIxMSIsInVzZXJfaWQiOjV9.hW-PSDKWGxn0qxfdpo0WVLlg8-_CDuOZPrJgqkywARs'
+
+            headers = {
+              "Content-Type": "application/json",
+              "Authorization": AUTH_TOKEN
+            }
+
+            print (headers)
+
+            response = requests.request("POST", url, headers=headers, data=payload);
+
+            print (response)
+
 
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
