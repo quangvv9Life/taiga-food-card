16fcf1089d3fd120e77077a4dd738f876777c949
9Health <ninehealth at outlook dot com>
2022-06-01 11:59:30 +0700

    Refactoring code. Use smaller function.

--- 02_db_select.py
+++ 02_db_select.py
@@ -7,48 +7,95 @@ import psycopg2
 import json
 import requests
 
+##################################################
+# Global for requests
+##################################################
+
+AUTH_TOKEN = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjU0MDc5NDA0LCJqdGkiOiI4YzdlY2ZmOTA0YTI0MmJhYmMzNzA2MGEzMzkyYmIxMSIsInVzZXJfaWQiOjV9.hW-PSDKWGxn0qxfdpo0WVLlg8-_CDuOZPrJgqkywARs'
+
+headers = {
+  "Content-Type": "application/json",
+  "Authorization": AUTH_TOKEN
+}
+
+
+url_user_stories = "http://9net.ddns.net:9000/api/v1/userstories"
+
+##################################################
+# Global for database
+##################################################
+
+""" select food """
+
+sql1 = """
+    CREATE VIEW LessThan5wIngre AS (
+    SELECT a.id,a.Name,a.TotalTime,b.Name AS IngreName,b.Id AS IngreID,a.TotalLiked,a.AvgRating,a.DetailUrl
+    FROM ingredientref r
+    JOIN LessThanFive a  ON a.id= r.cookyfoodid
+    JOIN ingredient b ON r.ingredientid = b.id
+    GROUP BY a.name,a.TotalTime,a.Id,b.Name ,b.Id,a.TotalLiked,a.AvgRating,a.DetailUrl
+    ORDER BY id
+    );
+"""
+
+sql2 = """
+
+   SELECT DISTINCT LT5WI.id,LT5WI.name, ig.NameVIE,ig.NameENG,ig.FinalName,ig.OriginalWeight,ig.Calucateweight, ig.Calucateunit,ig.MaxCalucateWeight,ig.SubstituteIds,nu.unit,nu.isverified,
+
+          UNNEST(ARRAY[
+                 'nu.calories','nu.sodium','nu.totalCarbs','nu.totalFat','nu.potassium','nu.saturated','nu.monounsaturated','nu.polyunsaturated','nu.dietaryFiber','nu.sugars','nu.trans','nu.protein','nu.cholesterol',
+                 'nu.vitaminA','nu.vitaminC','nu.calcium','nu.iron']
+          ) AS Nutritions,
+
+          UNNEST(ARRAY[
+                 nu.calories,nu.sodium,nu.totalCarbs,nu.totalFat,nu.potassium,nu.saturated,nu.monounsaturated,nu.polyunsaturated,nu.dietaryFiber,nu.sugars,nu.trans,nu.protein,nu.cholesterol,
+                 nu.vitaminA,nu.vitaminC,nu.calcium,nu.iron]
+          ) AS Values
+
+   FROM LessThan5wIngre LT5WI
+    JOIN IngredientRef re ON LT5WI.id = re.CookyFoodId
+    JOIN IngredientTrans ig ON re.IngredientId = ig.Id
+    JOIN IngredientNutrient nu ON ig.NameENG = nu.key
+    WHERE 1=1
+   ORDER BY id;
+
+"""
+
+sql3 = """
+     SELECT * FROM LessThan5wIngre ;
+"""
+
+##################################################
+# Functions
+##################################################
+
+def send_card(i_url, i_payload):
+    o_response = requests.request("POST", i_url, headers=headers, data=i_payload);
+
+    print (headers)
+    print (o_response)
+
+    return o_response
+
+def parse_data(i_row):
+    out = {}
+    out['subject']   = i_row[0][1]
+    out['nutrition'] = i_row[0][12]
+
+    return out
+
+def prepare_card(i_project_id, i_subject):
+
+    o_payload = json.dumps({
+        "project" : i_project_id      ,
+        "subject" : i_subject 
+    })
+
+    print (o_payload)
+
+    return o_payload
+
 def select():
-
-    """ select food """
-
-    sql1 = """
-        CREATE VIEW LessThan5wIngre AS (
-        SELECT a.id,a.Name,a.TotalTime,b.Name AS IngreName,b.Id AS IngreID,a.TotalLiked,a.AvgRating,a.DetailUrl
-        FROM ingredientref r
-        JOIN LessThanFive a  ON a.id= r.cookyfoodid
-        JOIN ingredient b ON r.ingredientid = b.id
-        GROUP BY a.name,a.TotalTime,a.Id,b.Name ,b.Id,a.TotalLiked,a.AvgRating,a.DetailUrl
-        ORDER BY id
-        );
-    """
-
-    sql2 = """
-
-       SELECT DISTINCT LT5WI.id,LT5WI.name, ig.NameVIE,ig.NameENG,ig.FinalName,ig.OriginalWeight,ig.Calucateweight, ig.Calucateunit,ig.MaxCalucateWeight,ig.SubstituteIds,nu.unit,nu.isverified,
-
-              UNNEST(ARRAY[
-                     'nu.calories','nu.sodium','nu.totalCarbs','nu.totalFat','nu.potassium','nu.saturated','nu.monounsaturated','nu.polyunsaturated','nu.dietaryFiber','nu.sugars','nu.trans','nu.protein','nu.cholesterol',
-                     'nu.vitaminA','nu.vitaminC','nu.calcium','nu.iron']
-              ) AS Nutritions,
-
-              UNNEST(ARRAY[
-                     nu.calories,nu.sodium,nu.totalCarbs,nu.totalFat,nu.potassium,nu.saturated,nu.monounsaturated,nu.polyunsaturated,nu.dietaryFiber,nu.sugars,nu.trans,nu.protein,nu.cholesterol,
-                     nu.vitaminA,nu.vitaminC,nu.calcium,nu.iron]
-              ) AS Values
-
-       FROM LessThan5wIngre LT5WI
-        JOIN IngredientRef re ON LT5WI.id = re.CookyFoodId
-        JOIN IngredientTrans ig ON re.IngredientId = ig.Id
-        JOIN IngredientNutrient nu ON ig.NameENG = nu.key
-        WHERE 1=1
-       ORDER BY id;
-
-    """
-
-    sql3 = """
-         SELECT * FROM LessThan5wIngre ;
-    """
-
     conn = None
     updated_rows = 0
 
@@ -77,30 +124,18 @@ def select():
             # print (curs.fetchmany(20))
             # print (type(curs.fetchmany(20)))
 
-            row = curs.fetchmany(1)
+            row_fetch = 4
+            row = curs.fetchmany(row_fetch)
 
-            payload = json.dumps( {
-                "project" : "3"       ,
-                "subject" : row[0][1]
-            } )
+            print (row)
 
-            print (payload)
+            for i in range(row_fetch):
+                out_data   = parse_data(row)
 
-            url = "http://9net.ddns.net:9000/api/v1/userstories"
+                project_id = 3
+                payload = prepare_card(project_id, out_data['subject'])
 
-            AUTH_TOKEN = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjU0MDc5NDA0LCJqdGkiOiI4YzdlY2ZmOTA0YTI0MmJhYmMzNzA2MGEzMzkyYmIxMSIsInVzZXJfaWQiOjV9.hW-PSDKWGxn0qxfdpo0WVLlg8-_CDuOZPrJgqkywARs'
-
-            headers = {
-              "Content-Type": "application/json",
-              "Authorization": AUTH_TOKEN
-            }
-
-            print (headers)
-
-            response = requests.request("POST", url, headers=headers, data=payload);
-
-            print (response)
-
+               #send_card(url_user_stories, payload)
 
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
@@ -111,7 +146,11 @@ def select():
 
     return updated_rows
 
+
+##################################################
+# Main
+##################################################
+
 if __name__ == '__main__':
-    # Update vendor id 1
     select()
 
